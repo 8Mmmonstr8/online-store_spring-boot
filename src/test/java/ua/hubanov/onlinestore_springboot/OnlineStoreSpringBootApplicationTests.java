1ae767b61cc3da44bee7d5df33c2b54c818216e1
Pavlo Hubanov <daw2025@gmail.com>
2020-11-25 19:17:59 +0200

    added online-store project

---
+++ src/test/java/ua/hubanov/onlinestore_springboot/OnlineStoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hubanov.onlinestore_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
