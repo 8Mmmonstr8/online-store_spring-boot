49edf7fcff55866d9a5263674f5142e1b6298da2
Pavlo Hubanov <daw2025@gmail.com>
2020-11-26 13:19:23 +0200

    return Lombooks annotaiton and removed getters and setters

--- src/main/java/ua/hubanov/onlinestore_springboot/entity/User.java
+++ src/main/java/ua/hubanov/onlinestore_springboot/entity/User.java
@@ -4,12 +4,12 @@ import lombok.*;
 
 import javax.persistence.*;
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 
 @Entity
 @Table( name="user",
@@ -29,49 +29,14 @@ public class User {
     @Column(nullable = false)
     private String email;
 
-    public User() {
-    }
+    @Column(name = "password", nullable = false)
+    private String password;
 
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    //    @Column(name = "role")
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private UserRole role = UserRole.USER;
+//        @Column(name = "role")
 //    @Enumerated(EnumType.STRING)
 //    private UserRole role;
 }
