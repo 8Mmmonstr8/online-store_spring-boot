44c1eed84d173916d746cb39494c4103806ff2b9
Pavlo Hubanov <daw2025@gmail.com>
2020-11-26 02:26:32 +0200

    deleted MainController.java and created UserController instead

---
+++ src/main/java/ua/hubanov/onlinestore_springboot/controllers/UserController.java
@@ -0,0 +1,54 @@
+package ua.hubanov.onlinestore_springboot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.hubanov.onlinestore_springboot.entity.User;
+import ua.hubanov.onlinestore_springboot.repository.UserRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "user/home";
+    }
+
+    @GetMapping("/temp-members-page")
+    public String temp(Model model) {
+        model.addAttribute("title", "Пользователи");
+
+        List<User> list = userRepository.findAll();
+        model.addAttribute("users", list);
+
+        return "user/temp_members_page";
+    }
+
+    @GetMapping("/registration")
+    public String newPerson(@ModelAttribute("user") User user) {
+        return "user/registration";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+
+        userRepository.save(user);
+        return "redirect:/user/";
+    }
+
+}
