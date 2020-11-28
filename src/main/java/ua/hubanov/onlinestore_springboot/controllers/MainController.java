cba3df9c5b7ccd9f7f63239d17e73fdd43289edf
Pavlo Hubanov <daw2025@gmail.com>
2020-11-28 22:42:09 +0200

    renew main page view

---
+++ src/main/java/ua/hubanov/onlinestore_springboot/controllers/MainController.java
@@ -0,0 +1,15 @@
+package ua.hubanov.onlinestore_springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Maaain page");
+        return "index";
+    }
+}
