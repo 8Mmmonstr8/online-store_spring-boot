cdf4f7327b26dc70c5c1f7f3a4bf8bcd2f1e0afd
Pavlo Hubanov <daw2025@gmail.com>
2020-11-30 05:10:37 +0200

    added users page, updated admin controller, added user_edit_form

--- src/main/java/ua/hubanov/onlinestore_springboot/controllers/AdminController.java
+++ src/main/java/ua/hubanov/onlinestore_springboot/controllers/AdminController.java
@@ -2,10 +2,13 @@ package ua.hubanov.onlinestore_springboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.hubanov.onlinestore_springboot.entity.User;
 import ua.hubanov.onlinestore_springboot.repository.UserRepository;
 import ua.hubanov.onlinestore_springboot.service.UserService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -22,5 +25,36 @@ public class AdminController {
     @GetMapping("")
     public String adminHome() {
         return "/admin/admin_home";
+    }
+
+    @GetMapping("/users")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "/admin/users";
+    }
+
+    @GetMapping("/users/{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+//        Optional<User> userFromBd = userRepository.findById(id);
+//        //todo
+//        User user = userFromBd.get();
+        model.addAttribute("user", user);
+        String s = String.valueOf(user.isAccountNonLocked());
+        model.addAttribute("isActive", s);
+        return "/admin/user_edit_form";
+    }
+
+    @PostMapping("/users")
+    public String userUpdate(
+            @RequestParam boolean active,
+            @RequestParam String firstName,
+            @RequestParam("id") User user) {
+
+        if (active == false)
+            user.setAccountLocked();
+
+        userRepository.save(user);
+
+        return "redirect:/admin/users";
     }
 }
